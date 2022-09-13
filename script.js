6db3e02407990f28f85388f9fe8a078b9a8b75b3
the legend <legendre.oli@gmail.com>
2022-09-13 16:24:29 -0400

    make books appear right in the books__shelf

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function Book(title, collection, author, year, status) {
     this.status = status
 }
 
+// initialize
+refreshLib()
+
 // rudimentary
 function addBookToLibrary() {
   let newBook = new Book(prompt("title"), prompt("collection"), prompt("author"), prompt("year"), prompt("status"))
@@ -18,30 +21,66 @@ function addBookToLibrary() {
 }
 
 
-/*
-refresh lib function
-
-- target the container of "book__container", "books__shelf"
-- in "books__shelf", create a new div of a class "book__container"
-- in the new "book__container", create a div with a class of "book__title"
-- set the textContent of that div to book.title
-
-
-
-*/
-
 function refreshLib() {
-  alert("it works")
+  // alert("it works") ! 
+  
   let booksShelf = document.querySelector(".books__shelf")
 
   myLibrary.forEach(book => {
+    // create the container
     let bookContainer = document.createElement("div")
     bookContainer.classList.add("book__container")
     booksShelf.appendChild(bookContainer);
 
-  })
+    // TODO: Create a function to streamline the following
+    // createBookElements("book__title", "bookTitle")
 
-}
+    // function createBookElements(htmlClass, jsVar) {
+    //   let jsVar = document.createElement("div")
+    //   bookTitle.classList.add(htmlClass)
+    //   bookContainer.appendChild(jsVar);
+    // }
+
+    // create the elements 
+    let bookTitle = document.createElement("div")
+    bookTitle.classList.add("book__title")
+    bookContainer.appendChild(bookTitle);
+    bookTitle.textContent = book.title;
+
+    let bookAuthor = document.createElement("div")
+    bookAuthor.classList.add("book__author")
+    bookContainer.appendChild(bookAuthor);
+    bookAuthor.textContent = book.author;
+
+    let bookCollection = document.createElement("div")
+    bookCollection.classList.add("book__collection")
+    bookContainer.appendChild(bookCollection);
+    bookCollection.textContent = book.collection;
+
+    let bookYear = document.createElement("div")
+    bookYear.classList.add("book__year")
+    bookContainer.appendChild(bookYear);
+    bookYear.textContent = book.year;
+
+    let bookStatus = document.createElement("div")
+    bookStatus.classList.add("book__status", "btn")
+    bookContainer.appendChild(bookStatus);
+    bookStatus.textContent = book.status;
+
+    //  TODO: Use the nullish coalescing operator for this!
+    // check book read/unread status
+    if (book.status === "read") {
+      bookStatus.classList.add("is-read")
+    } else if (book.status === "unread") {
+      bookStatus.classList.add("is-unread")
+    }
+
+  
+
+  });
+  refreshButtons()
+};
+
 
 
 
@@ -55,44 +94,49 @@ testingButton.addEventListener("click", () => refreshLib());
 const btnAddBook = document.querySelector(".books__btn-add-book");
 btnAddBook.addEventListener("click", addBookToLibrary);
 
+
+function refreshButtons() {
+  // read/unread
+  const readingStatus = document.querySelectorAll(".book__status")
+  readingStatus.forEach((button) => {
+
+    // ! this changes the appearance but not the "status" property in myLibrary
+    // ! but I don't know how to change that yet
+    button.addEventListener("click", function checkReadingStatus() {
+      if (button.classList.contains("is-read")) {
+        button.textContent = "Unread"
+        button.classList.remove("is-read")
+        button.classList.add("is-unread")
+      } else if (button.classList.contains("is-unread")) {
+        button.textContent = "Read"
+        button.classList.remove("is-unread")
+        button.classList.add("is-read")
+      }
+    });
+  });
+}
+
+
+
+
 // add demo data
 const btnAddDemoData = document.querySelector(".left-panel__btn-add-data")
 btnAddDemoData.addEventListener("click", addDemoDataToLibrary)
 
-// read/unread
-const readingStatus = document.querySelectorAll(".book__status")
-readingStatus.forEach((button) => {
-
-  // this changes the appearance but not the "status" property in myLibrary
-  button.addEventListener("click", function checkReadingStatus() {
-    if (button.classList.contains("is-read")) {
-      button.textContent = "Unread"
-      button.classList.remove("is-read")
-      button.classList.add("is-unread")
-    } else if (button.classList.contains("is-unread")) {
-      button.textContent = "Read"
-      button.classList.remove("is-unread")
-      button.classList.add("is-read")
-    }
-  });
-});
-
-
-
-
-
-
 function addDemoDataToLibrary() {
-  addDemoBooks("Skyward", "Skyward", "Brandon Sanderson", 2018, "read")
-  addDemoBooks("Steelheart", "Brandon Sanderson", "The Reckoners", 2013, "read")
-  addDemoBooks("Firefight", "Brandon Sanderson", "The Reckoners", 2015, "read")
-  addDemoBooks("A Deadly Education", "Naomi Novik", "The Scholomance", 2020, "read")
-  addDemoBooks("Pride and Prejudice", "Jane Austen", "", 1813, "read")
+  addDemoBooks("Skyward", "Skyward", "Brandon Sanderson", 2018, "unread")
+  addDemoBooks("Steelheart", "The Reckoners", "Brandon Sanderson", 2013, "read")
+  addDemoBooks("Firefight", "The Reckoners", "Brandon Sanderson", 2015, "read")
+  addDemoBooks("A Deadly Education", "The Scholomance", "Naomi Novik", 2020, "unread")
+  addDemoBooks("Pride and Prejudice", "", "Jane Austen", 1813, "read")
+  addDemoBooks("Great Expectations", "", "Charles Dickens", 1860, "read")
+  addDemoBooks("Eldest", "The Inheritance Cycle", "Christopher Paolini", 2005, "unread")
+  addDemoBooks("Brisingr", "The Inheritance Cycle", "Christopher Paolini", 2008, "read")
 
   function addDemoBooks(title, collection, author, year, status) {
     let demoBook = new Book(title, collection, author, year, status)
     myLibrary.push(demoBook)
   }
 
-
-}
+  refreshLib();
+};

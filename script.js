4ec8d4f042e0e9b5f181ea2e29b998dc76f482dd
the legend <legendre.oli@gmail.com>
2022-09-21 17:16:47 -0400

    made the modal work

--- script.js
+++ script.js
@@ -9,43 +9,22 @@ function Book(title, collection, author, year, status) {
     this.status = status
 }
 
-function Collection(name, author) {
-  this.name = name;
-  this.author = author;
-  this.numOfBooks = 0;
-}
-
 // initialize
 addDemoDataToLibrary();
-
-// rudimentary function, will change 
-function addBookToLibrary() {
-  let newBook = new Book(prompt("title"), prompt("collection"), prompt("author"), prompt("year"), prompt("status"))
-  myLibrary.push(newBook);
-  console.log(newBook.title + " has been added to your library.")
-  refreshLib();
-}
 
 
 // refresh interface so that what you see is the actual content of myLibrary
 function refreshLib() {
   // alert("it works")  
   let booksShelf = document.querySelector(".books__shelf")
+  // start afresh everytime
+  removeAllChildNodes(booksShelf);
 
   myLibrary.forEach(book => {
     // create the container
     let bookContainer = document.createElement("div")
     bookContainer.classList.add("book__container")
     booksShelf.appendChild(bookContainer);
-
-    // TODO: Create a function to streamline the following
-    // createBookElements("book__title", "bookTitle")
-
-    // function createBookElements(htmlClass, jsVar) {
-    //   let jsVar = document.createElement("div")
-    //   bookTitle.classList.add(htmlClass)
-    //   bookContainer.appendChild(jsVar);
-    // }
 
     // create the elements 
     let bookTitle = document.createElement("div")
@@ -74,9 +53,9 @@ function refreshLib() {
     bookStatus.textContent = book.status;
 
     // check book read/unread status
-    if (book.status.toLowerCase() === "read") {
+    if (book.status == "read") {
       bookStatus.classList.add("is-read")
-    } else if (book.status.toLowerCase() === "unread") {
+    } else if (book.status == "unread") {
       bookStatus.classList.add("is-unread")
     }
 
@@ -97,12 +76,11 @@ function refreshLib() {
 const testingButton = document.querySelector(".logo")
 testingButton.addEventListener("click", () => {
   console.log("Testing button is working.")
+
 });
 
 
-// button: add books 
-const btnAddBook = document.querySelector(".books__btn-add-book");
-btnAddBook.addEventListener("click", addBookToLibrary);
+
 
 // button: toggle read/unread on each book
 function toggleReadUnread() {
@@ -164,25 +142,96 @@ function toggleReadUnread() {
 
 
 
-// add demo data
+
+
+// modal
+
+// button: add books 
+const openModalButton = document.querySelector(".books__btn-add-book");
+const closeModalButton = document.getElementById("cancel")
+const overlay = document.getElementById("overlay")
+const modal = document.querySelector(".form-container")
+const form = document.querySelector(".form")
+const submitButton = document.querySelector("#submit")
+
+
+
+openModalButton.addEventListener("click", () => openModal(modal));
+closeModalButton.addEventListener("click", () => closeModal(modal));
+
+submitButton.addEventListener("click", () => {
+
+  let title = document.querySelector("#title")
+  let collection = document.querySelector("#collection")
+  let author = document.querySelector("#author")
+  let year = document.querySelector("#year")
+  let status = document.querySelector("input[type='radio']:checked").value;
+
+  let newBook = [];
+
+  // let newBook = new Book(title.value, collection.value, author.value, year.value, readingStatus.value)
+  newBook = new Book(title.value, collection.value, author.value, year.value, status)
+
+  myLibrary.push(newBook)
+  refreshLib();
+  console.log(newBook.title + " has been added to your library.")
+  closeModal(modal)
+});
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add("active")
+  overlay.classList.add("active")
+
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove("active")
+  overlay.classList.remove("active")
+
+  // reset fields
+  document.querySelector("#title").value = ""
+  document.querySelector("#collection").value = ""
+  document.querySelector("#author").value = ""
+  document.querySelector("#year").value = ""
+
+  // reset radio buttons
+  const readingStatus = document.getElementsByName("status");
+  for (let i = 0; i < readingStatus.length; i++) {
+    if (readingStatus[i].type == "radio") {
+      readingStatus[i].checked = false;
+    }
+  }
+
+}
+
+// 2.3 add demo data
 const btnAddDemoData = document.querySelector(".left-panel__btn-add-data")
 btnAddDemoData.addEventListener("click", addDemoDataToLibrary)
 
 function addDemoDataToLibrary() {
   console.log("Demo data has been added.")
   addDemoBooks("Skyward", "Skyward", "Brandon Sanderson", 2018, "unread")
-  addDemoBooks("Steelheart", "The Reckoners", "Brandon Sanderson", 2013, "read")
-  addDemoBooks("Firefight", "The Reckoners", "Brandon Sanderson", 2015, "read")
-  addDemoBooks("A Deadly Education", "The Scholomance", "Naomi Novik", 2020, "unread")
-  addDemoBooks("Golden Enclaves", "The Scholomance", "Naomi Novik", 2022, "unread")
-  addDemoBooks("Pride and Prejudice", "", "Jane Austen", 1813, "read")
-  addDemoBooks("Great Expectations", "", "Charles Dickens", 1860, "read")
-  addDemoBooks("Eldest", "The Inheritance Cycle", "Christopher Paolini", 2005, "unread")
-  addDemoBooks("Brisingr", "The Inheritance Cycle", "Christopher Paolini", 2008, "read")
+  // addDemoBooks("Steelheart", "The Reckoners", "Brandon Sanderson", 2013, "read")
+  // addDemoBooks("Firefight", "The Reckoners", "Brandon Sanderson", 2015, "read")
+  // addDemoBooks("A Deadly Education", "The Scholomance", "Naomi Novik", 2020, "unread")
+  // addDemoBooks("Golden Enclaves", "The Scholomance", "Naomi Novik", 2022, "unread")
+  // addDemoBooks("Pride and Prejudice", "", "Jane Austen", 1813, "read")
+  // addDemoBooks("Great Expectations", "", "Charles Dickens", 1860, "read")
+  // addDemoBooks("Eldest", "The Inheritance Cycle", "Christopher Paolini", 2005, "unread")
+  // addDemoBooks("Brisingr", "The Inheritance Cycle", "Christopher Paolini", 2008, "read")
 
   function addDemoBooks(title, collection, author, year, status) {
     let demoBook = new Book(title, collection, author, year, status)
     myLibrary.push(demoBook)
   };
   refreshLib()
+}
+
+// remove all child nodes
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
